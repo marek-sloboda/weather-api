8fa6ce951d1dea2441e4ded208e331c4ebe33ae7
marek słoboda <marek.sloboda@bandit.house>
2022-08-07 11:37:41 +0200

    redis fix added WeatherItemDataProvider.php, WeatherSubresourceDataProvider.php

--- src/DataProvider/WeatherItemDataProvider.php
+++ src/DataProvider/WeatherItemDataProvider.php
@@ -1,39 +1,32 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\DataProvider;
 
-
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
-use ApiPlatform\Core\DataProvider\SubresourceDataProviderInterface;
 use App\Entity\Weather;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 
-class WeatherItemDataProvider implements RestrictedDataProviderInterface, SubresourceDataProviderInterface
+class WeatherItemDataProvider implements ItemDataProviderInterface, RestrictedDataProviderInterface
 {
-    private bool $alreadyInvoked = false;
-
     public function __construct(
         private readonly CacheInterface         $cacheWeather,
-        private readonly EntityManagerInterface $em,
+        private readonly EntityManagerInterface $em
     )
     {
     }
 
-    public function getSubresource(string $resourceClass, array $identifiers, array $context, string $operationName = null): ?Weather
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ?Weather
     {
-        $this->alreadyInvoked = true;
-
-        return $this->cacheWeather->get('location-' . $context['subresource_identifiers']['id'], function (ItemInterface $item) use ($resourceClass, $identifiers, $context, $operationName) {
-            return $this->em->getRepository($resourceClass)->findOneBy(['location' => $context['subresource_identifiers']['id']]);
+        return $this->cacheWeather->get('weather_' . $id, function (ItemInterface $item) use ($resourceClass, $id) {
+            return $this->em->getRepository($resourceClass)->find($id);
         });
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
-        return !$this->alreadyInvoked && Weather::class === $resourceClass;
+        return Weather::class === $resourceClass;
     }
 }
---
+++ src/DataProvider/WeatherSubresourceDataProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataProvider;
+
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use ApiPlatform\Core\DataProvider\SubresourceDataProviderInterface;
+use App\Entity\Weather;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
+
+class WeatherSubresourceDataProvider implements RestrictedDataProviderInterface, SubresourceDataProviderInterface
+{
+    private bool $alreadyInvoked = false;
+
+    public function __construct(
+        private readonly CacheInterface         $cacheWeather,
+        private readonly EntityManagerInterface $em,
+    )
+    {
+    }
+
+    public function getSubresource(string $resourceClass, array $identifiers, array $context, string $operationName = null): ?Weather
+    {
+        $this->alreadyInvoked = true;
+
+        return $this->cacheWeather->get('location-' . $context['subresource_identifiers']['id'], function (ItemInterface $item) use ($resourceClass, $identifiers, $context, $operationName) {
+            return $this->em->getRepository($resourceClass)->findOneBy(['location' => $context['subresource_identifiers']['id']]);
+        });
+    }
+
+    public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
+    {
+        return !$this->alreadyInvoked && Weather::class === $resourceClass;
+    }
+}
